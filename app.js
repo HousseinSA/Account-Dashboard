36a2f19d00cba1e395f0e61c8cc0c9b9bb16bd98
hussein <nejihoussein1@gmail.com>
2022-08-18 16:02:09 +0000

    fix navbaa bug when click close btnh

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 // get the emlents
 const menuBtn = document.querySelector(".menu-btn")
 const Close = document.querySelector(".close")
-const navBar = document.querySelector("aside")
+const aside = document.querySelector("aside")
 const themeChange = document.querySelector(".theme-toggle")
 
 // event listener when clicking the menu btn
 menuBtn.addEventListener("click", () => {
-  navBar.style = "display:block"
-})
-// remove  the menu when scrolling
-window.addEventListener("scroll", () => {
-  // navBar.style = "display:none"
-})
-// event when clicking on close btn
-Close.addEventListener("click", () => {
-  navBar.style = "display:none"
+  aside.style = "display:block"
 })
 
-// changing the theme 
-themeChange.addEventListener("click",()=>{
+// event when clicking on close btn
+Close.addEventListener("click", () => {
+  aside.style = "display:none"
+  window.addEventListener("resize", () => {
+    aside.style = "display:block"
+  })
+})
+
+// changing the theme
+themeChange.addEventListener("click", () => {
   document.querySelector("body").classList.toggle("changingtheme")
   themeChange.querySelector("span:nth-child(1)").classList.toggle("active")
   themeChange.querySelector("span:nth-child(2)").classList.toggle("active")
-  
 })
 
-// add produts to table 
-products.map(product=>{
+// add produts to table
+products.map((product) => {
   const tr = document.createElement("tr")
   const tableDate = `
     <td>${product.productName}</td>
@@ -44,4 +43,3 @@ products.map(product=>{
   tr.innerHTML = tableDate
   document.querySelector("tbody").appendChild(tr)
 })
-
